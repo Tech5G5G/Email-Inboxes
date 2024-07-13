b53216ea445206517b36f25af19850df7d8e0607
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-07-12 18:58:38 -0700

    Made firstBootWindow start centered on screen

--- Email Inboxes/App.xaml.cs
+++ Email Inboxes/App.xaml.cs
@@ -152,6 +152,20 @@ namespace Email_Inboxes
             {
                 //If not, it creates and activates FirstBootWindow
                 Window firstBootWindow = new FirstBootWindow();
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(firstBootWindow);
+                Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+                Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                if (appWindow is not null)
+                {
+                    Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                    if (displayArea is not null)
+                    {
+                        var CenteredPosition = appWindow.Position;
+                        CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
+                        CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
+                        appWindow.Move(CenteredPosition);
+                    }
+                }
                 firstBootWindow.Activate();
             }
 
