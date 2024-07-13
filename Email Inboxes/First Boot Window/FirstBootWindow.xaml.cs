2a879ebb8c35a8a6e9f0207bcea8b49b41e7edec
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-07-12 19:21:45 -0700

    Removed maximize & minimize buttons

--- Email Inboxes/First Boot Window/FirstBootWindow.xaml.cs
+++ Email Inboxes/First Boot Window/FirstBootWindow.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +28,19 @@ namespace Email_Inboxes
         public FirstBootWindow()
         {
             this.InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+
+            var presenter = GetAppWindowAndPresenter();
+            presenter.IsMaximizable = false;
+            presenter.IsMinimizable = false;
+        }
+
+        private OverlappedPresenter GetAppWindowAndPresenter()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var _apw = AppWindow.GetFromWindowId(myWndId);
+            return _apw.Presenter as OverlappedPresenter;
         }
     }
 }
