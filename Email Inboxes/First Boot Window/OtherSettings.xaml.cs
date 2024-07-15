7b3ff26c1445da3cb4e8cc873fe82e7c428097bf
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-07-15 15:49:20 -0700

    Added usage

--- Email Inboxes/First Boot Window/OtherSettings.xaml.cs
+++ Email Inboxes/First Boot Window/OtherSettings.xaml.cs
@@ -13,6 +13,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Composition.SystemBackdrops;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
