a6865b5ef54d002901b64afcf7921cb59608eff8
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-08-14 12:34:44 +0200

    Uncommented code to launch FirstBootWindow

--- Email Inboxes/App.xaml.cs
+++ Email Inboxes/App.xaml.cs
@@ -147,33 +147,32 @@ namespace Email_Inboxes
                 localSettings.Values[Settings.VersionNumber] = "1.3";
 
             //Checks for value of FirstBootScreenPassed
-            //if ((bool)localSettings.Values[Settings.FirstBootScreenPassed])
-            //{
+            if ((bool)localSettings.Values[Settings.FirstBootScreenPassed])
+            {
                 //If true, it creates & activates the MainWindow
                 m_window = new MainWindow();
                 m_window.Activate();
-            //}
-            //else
-            //{
-            //    //If not, it creates and activates FirstBootWindow
-            //    firstBootWindow = new FirstBootWindow();
-            //    var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(firstBootWindow);
-            //    Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
-            //    Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-            //    if (appWindow is not null)
-            //    {
-            //        Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
-            //        if (displayArea is not null)
-            //        {
-            //            var CenteredPosition = appWindow.Position;
-            //            CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
-            //            CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
-            //            appWindow.Move(CenteredPosition);
-            //        }
-            //    }
-            //    firstBootWindow.Activate();
-            //}
-
+            }
+            else
+            {
+                //If not, it creates and activates FirstBootWindow
+                firstBootWindow = new FirstBootWindow();
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(firstBootWindow);
+                Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+                Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                if (appWindow is not null)
+                {
+                    Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                    if (displayArea is not null)
+                    {
+                        var CenteredPosition = appWindow.Position;
+                        CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
+                        CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
+                        appWindow.Move(CenteredPosition);
+                    }
+                }
+                firstBootWindow.Activate();
+            }
         }
 
         public Window m_window;
