543faa337fb028d558d77c4070f5cbad6fbb5db4
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-08-17 10:01:22 +0200

    Added SettingsCache class

--- Email Inboxes/First Boot Window/FirstBootWindow.xaml.cs
+++ Email Inboxes/First Boot Window/FirstBootWindow.xaml.cs
@@ -29,14 +29,132 @@ namespace Email_Inboxes
     /// </summary>
     public sealed partial class FirstBootWindow : Window
     {
+        public static class SettingsCache
+        {
+            public static string StartupPage
+            {
+                get { return StartupPage; }
+                set { StartupPage = value; }
+            }
+            private static string startupPage;
+
+            public static bool HomeEnabled
+            {
+                get { return homeEnabled; }
+                set { homeEnabled = value; }
+            }
+            private static bool homeEnabled;
+
+            public static bool OutlookEnabled
+            {
+                get { return outlookEnabled; }
+                set { outlookEnabled = value; }
+            }
+            private static bool outlookEnabled;
+
+            public static bool ProtonEnabled
+            {
+                get { return protonEnabled; }
+                set { protonEnabled = value; }
+            }
+            private static bool protonEnabled;
+
+            public static bool IcloudEnabled
+            {
+                get { return iCloudEnabled; }
+                set { iCloudEnabled = value; }
+            }
+            private static bool iCloudEnabled;
+
+            public static bool GmailEnabled
+            {
+                get { return gmailEnabled; }
+                set { gmailEnabled = value; }
+            }
+            private static bool gmailEnabled;
+
+            public static bool YahooEnabled
+            {
+                get { return yahooEnabled; }
+                set { yahooEnabled = value; }
+            }
+            private static bool yahooEnabled;
+
+            public static bool CommandBarEnabled
+            {
+                get { return commandBarEnabled; }
+                set { commandBarEnabled = value; }
+            }
+            private static bool commandBarEnabled;
+
+            public static string OutlookAppType
+            {
+                get { return outlookAppType; }
+                set { outlookAppType = value; }
+            }
+            private static string outlookAppType;
+
+            public static string ToDoServiceName
+            {
+                get { return toDoServiceName; }
+                set { toDoServiceName = value; }
+            }
+            private static string toDoServiceName;
+
+            public static string CalendarServiceName
+            {
+                get { return calendarServiceName; }
+                set { calendarServiceName = value; }
+            }
+            private static string calendarServiceName;
+
+            public static string ToDoServiceUrl
+            {
+                get { return toDoServiceUrl; }
+                set { toDoServiceUrl = value; }
+            }
+            private static string toDoServiceUrl;
+
+            public static string CalendarServiceUrl
+            {
+                get { return calendarServiceUrl; }
+                set { calendarServiceUrl = value; }
+            }
+            private static string calendarServiceUrl;
+
+            public static string OutlookExePath
+            {
+                get { return outlookExePath; }
+                set { outlookExePath = value; }
+            }
+            private static string outlookExePath;
+
+            public static string Backdrop
+            {
+                get { return backdrop; }
+                set { backdrop = value; }
+            }
+            private static string backdrop;
+
+            public static string PaneDisplayMode
+            {
+                get { return paneDisplayMode; }
+                set { paneDisplayMode = value; }
+            }
+            private static string paneDisplayMode;
+        }
+            
+        OverlappedPresenter presenter;
+
         public FirstBootWindow()
         {
             this.InitializeComponent();
             ExtendsContentIntoTitleBar = true;
 
-            var presenter = GetAppWindowAndPresenter();
+            presenter = GetAppWindowAndPresenter();
             presenter.IsMaximizable = false;
             presenter.IsMinimizable = false;
+            presenter.IsResizable = false;
 
             logoStart.Loaded += StartAnimation;
 
