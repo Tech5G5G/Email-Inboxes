405abd40cd0167c5720ee5514cbbff901adffb6e
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-05-05 17:41:42 -0700

    Use setting classes

--- Email Inboxes/Settings/SettingValues.cs
+++ Email Inboxes/Settings/SettingValues.cs
@@ -1,329 +1,52 @@
 ﻿namespace Email_Inboxes.Settings;
 
-namespace Email_Inboxes.Settings
+public class SettingValues
 {
-    public class SettingValues
-    {
-        private static ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+    #region Services
 
-        public static PageType StartupPage
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("StartupPage", out object value))
-                    return Enum.Parse<PageType>((string)value);
-                else
-                {
-                    localSettings.Values["StartupPage"] = PageType.Home;
-                    return PageType.Home;
-                }
-            }
-            set
-            {
-                localSettings.Values["StartupPage"] = value.ToString();
-                StartupPageChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler StartupPageChanged;
+    public static Setting<bool> HomeEnabled = new(nameof(HomeEnabled), true);
 
-        public static bool HomeEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("HomeEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["HomeEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["HomeEnabled"] = value.ToString();
-                HomeEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler HomeEnabledChanged;
+    public static Setting<bool> GmailEnabled = new(nameof(GmailEnabled), false);
 
-        public static bool OutlookEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("OutlookEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["OutlookEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["OutlookEnabled"] = value.ToString();
-                OutlookEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler OutlookEnabledChanged;
+    public static Setting<bool> iCloudEnabled = new(nameof(iCloudEnabled), false);
 
-        public static bool ProtonEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("ProtonEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["ProtonEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["ProtonEnabled"] = value.ToString();
-                ProtonEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler ProtonEnabledChanged;
+    public static Setting<bool> ProtonEnabled = new(nameof(ProtonEnabled), false);
 
-        public static bool iCloudEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("iCloudEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["iCloudEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["iCloudEnabled"] = value.ToString();
-                iCloudEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler iCloudEnabledChanged;
+    public static Setting<bool> YahooEnabled = new(nameof(YahooEnabled), false);
 
-        public static bool GmailEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("GmailEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["GmailEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["GmailEnabled"] = value.ToString();
-                GmailEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler GmailEnabledChanged;
+    public static Setting<bool> OutlookEnabled = new(nameof(OutlookEnabled), false);
 
-        public static bool YahooEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("YahooEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["YahooEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["YahooEnabled"] = value.ToString();
-                YahooEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler YahooEnabledChanged;
+    public static EnumSetting<OutlookType> OutlookAppType = new(nameof(OutlookAppType), OutlookType.Website);
 
-        public static bool CommandBarEnabled
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("CommandBarEnabled", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["CommandBarEnabled"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["CommandBarEnabled"] = value.ToString();
-                CommandBarEnabledChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler CommandBarEnabledChanged;
+    public static Setting<string> OutlookExePath = new(nameof(OutlookExePath), @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE");
 
-        public static OutlookType OutlookAppType
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("OutlookAppType", out object value))
-                    return Enum.Parse<OutlookType>((string)value);
-                else
-                {
-                    localSettings.Values["OutlookAppType"] = OutlookType.Website.ToString();
-                    return OutlookType.Website;
-                }
-            }
-            set
-            {
-                localSettings.Values["OutlookAppType"] = value.ToString();
-                OutlookAppTypeChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler OutlookAppTypeChanged;
+    #endregion
 
-        public static CalendarService CalendarService
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("CalendarService", out object value))
-                    return Enum.Parse<CalendarService>((string)value);
-                else
-                {
-                    localSettings.Values["CalendarService"] = CalendarService.None.ToString();
-                    return CalendarService.None;
-                }
-            }
-            set
-            {
-                localSettings.Values["CalendarService"] = value.ToString();
-                CalendarServiceChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler CalendarServiceChanged;
+    #region Home
 
-        public static ToDoService ToDoService
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("ToDoService", out object value))
-                    return Enum.Parse<ToDoService>((string)value);
-                else
-                {
-                    localSettings.Values["ToDoService"] = ToDoService.None.ToString();
-                    return ToDoService.None;
-                }
-            }
-            set
-            {
-                localSettings.Values["ToDoService"] = value.ToString();
-                ToDoServiceChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler ToDoServiceChanged;
+    public static EnumSetting<CalendarService> CalendarService = new(nameof(CalendarService), Settings.CalendarService.None);
 
-        public static string OutlookExePath
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("OutlookExePath", out object value))
-                    return (string)value;
-                else
-                {
-                    localSettings.Values["OutlookExePath"] = @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";
-                    return @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";
-                }
-            }
-            set
-            {
-                localSettings.Values["OutlookExePath"] = value.ToString();
-                OutlookExePathChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler OutlookExePathChanged;
+    public static EnumSetting<ToDoService> ToDoService = new(nameof(ToDoService), Settings.ToDoService.None);
 
-        public static BackdropType Backdrop
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("Backdrop", out object value))
-                    return Enum.Parse<BackdropType>((string)value);
-                else
-                {
-                    localSettings.Values["Backdrop"] = BackdropType.Mica.ToString();
-                    return BackdropType.Mica;
-                }
-            }
-            set
-            {
-                localSettings.Values["Backdrop"] = value.ToString();
-                BackdropChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler BackdropChanged;
+    public static Setting<bool> CommandBarEnabled = new(nameof(CommandBarEnabled), true);
 
-        public static NavigationViewPaneDisplayMode PaneDisplayMode
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("PaneDisplayMode", out object value))
-                    return Enum.Parse<NavigationViewPaneDisplayMode>((string)value);
-                else
-                {
-                    localSettings.Values["PaneDisplayMode"] = NavigationViewPaneDisplayMode.Auto.ToString();
-                    return NavigationViewPaneDisplayMode.Auto;
-                }
-            }
-            set
-            {
-                localSettings.Values["PaneDisplayMode"] = value.ToString();
-                PaneDisplayModeChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler PaneDisplayModeChanged;
+    #endregion
 
-        public static bool FirstBootScreenPassed
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("FirstBootScreenPassed", out object value))
-                    return (bool)value;
-                else
-                {
-                    localSettings.Values["FirstBootScreenPassed"] = true;
-                    return true;
-                }
-            }
-            set
-            {
-                localSettings.Values["FirstBootScreenPassed"] = value.ToString();
-                FirstBootScreenPassedChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler FirstBootScreenPassedChanged;
+    #region Appearance
 
-        public static OverlappedPresenterState WindowState
-        {
-            get
-            {
-                if (localSettings.Values.TryGetValue("WindowState", out object value))
-                    return Enum.Parse<OverlappedPresenterState>((string)value);
-                else
-                {
-                    localSettings.Values["WindowState"] = OverlappedPresenterState.Restored.ToString();
-                    return OverlappedPresenterState.Restored;
-                }
-            }
-            set
-            {
-                localSettings.Values["WindowState"] = value.ToString();
-                WindowStateChanged?.Invoke(new SettingChangedEventArgs(value));
-            }
-        }
-        public static event SettingChangedEventHandler WindowStateChanged;
-    }
+    public static EnumSetting<PageType> StartupPage = new(nameof(StartupPage), PageType.Home);
+
+    public static EnumSetting<BackdropType> Backdrop = new(nameof(Backdrop), BackdropType.Mica);
+
+    public static EnumSetting<NavigationViewPaneDisplayMode> PaneDisplayMode = new(nameof(PaneDisplayMode), NavigationViewPaneDisplayMode.Auto);
+
+    #endregion
+
+    #region States
+
+    public static Setting<bool> FirstBootScreenPassed = new(nameof(FirstBootScreenPassed), false);
+
+    public static Setting<OverlappedPresenterState> WindowState = new(nameof(WindowState), OverlappedPresenterState.Restored);
+
+    #endregion
 }
