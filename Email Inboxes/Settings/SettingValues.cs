0269ba4552cef71baf743089d00587e02c1b980f
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-05-10 12:03:35 -0700

    Use properties instead of fields

--- Email Inboxes/Settings/SettingValues.cs
+++ Email Inboxes/Settings/SettingValues.cs
@@ -4,49 +4,49 @@ public class SettingValues
 {
     #region Services
 
-    public static Setting<bool> HomeEnabled = new(nameof(HomeEnabled), true);
+    public static Setting<bool> HomeEnabled { get; } = new(nameof(HomeEnabled), true);
 
-    public static Setting<bool> GmailEnabled = new(nameof(GmailEnabled), false);
+    public static Setting<bool> GmailEnabled { get; } = new(nameof(GmailEnabled), false);
 
-    public static Setting<bool> iCloudEnabled = new(nameof(iCloudEnabled), false);
+    public static Setting<bool> iCloudEnabled { get; } = new(nameof(iCloudEnabled), false);
 
-    public static Setting<bool> ProtonEnabled = new(nameof(ProtonEnabled), false);
+    public static Setting<bool> ProtonEnabled { get; } = new(nameof(ProtonEnabled), false);
 
-    public static Setting<bool> YahooEnabled = new(nameof(YahooEnabled), false);
+    public static Setting<bool> YahooEnabled { get; } = new(nameof(YahooEnabled), false);
 
-    public static Setting<bool> OutlookEnabled = new(nameof(OutlookEnabled), false);
+    public static Setting<bool> OutlookEnabled { get; } = new(nameof(OutlookEnabled), false);
 
-    public static EnumSetting<OutlookType> OutlookAppType = new(nameof(OutlookAppType), OutlookType.Website);
+    public static EnumSetting<OutlookType> OutlookAppType { get; } = new(nameof(OutlookAppType), OutlookType.Website);
 
-    public static Setting<string> OutlookExePath = new(nameof(OutlookExePath), @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE");
+    public static Setting<string> OutlookExePath { get; } = new(nameof(OutlookExePath), @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE");
 
     #endregion
 
     #region Home
 
-    public static EnumSetting<CalendarService> CalendarService = new(nameof(CalendarService), Settings.CalendarService.None);
+    public static EnumSetting<CalendarService> CalendarService { get; } = new(nameof(CalendarService), Settings.CalendarService.None);
 
-    public static EnumSetting<ToDoService> ToDoService = new(nameof(ToDoService), Settings.ToDoService.None);
+    public static EnumSetting<ToDoService> ToDoService { get; } = new(nameof(ToDoService), Settings.ToDoService.None);
 
-    public static Setting<bool> CommandBarEnabled = new(nameof(CommandBarEnabled), true);
+    public static Setting<bool> CommandBarEnabled { get; } = new(nameof(CommandBarEnabled), true);
 
     #endregion
 
     #region Appearance
 
-    public static EnumSetting<PageType> StartupPage = new(nameof(StartupPage), PageType.Home);
+    public static EnumSetting<PageType> StartupPage { get; } = new(nameof(StartupPage), PageType.Home);
 
-    public static EnumSetting<BackdropType> Backdrop = new(nameof(Backdrop), BackdropType.Mica);
+    public static EnumSetting<BackdropType> Backdrop { get; } = new(nameof(Backdrop), BackdropType.Mica);
 
-    public static EnumSetting<NavigationViewPaneDisplayMode> PaneDisplayMode = new(nameof(PaneDisplayMode), NavigationViewPaneDisplayMode.Auto);
+    public static EnumSetting<NavigationViewPaneDisplayMode> PaneDisplayMode { get; } = new(nameof(PaneDisplayMode), NavigationViewPaneDisplayMode.Auto);
 
     #endregion
 
     #region States
 
-    public static Setting<bool> FirstBootScreenPassed = new(nameof(FirstBootScreenPassed), false);
+    public static Setting<bool> FirstBootScreenPassed { get; } = new(nameof(FirstBootScreenPassed), false);
 
-    public static Setting<OverlappedPresenterState> WindowState = new(nameof(WindowState), OverlappedPresenterState.Restored);
+    public static EnumSetting<OverlappedPresenterState> WindowState { get; } = new(nameof(WindowState), OverlappedPresenterState.Restored);
 
     #endregion
 }
