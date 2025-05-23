6114cf2eaa388337a706ca0fd38367823af8820f
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-05-23 14:52:56 -0700

    Use properties instead of fields

--- Email Inboxes/Controls/InboxButton.xaml.cs
+++ Email Inboxes/Controls/InboxButton.xaml.cs
@@ -7,49 +7,44 @@ namespace Email_Inboxes.Controls
             this.InitializeComponent();
             this.DefaultStyleKey = typeof(InboxButton);
 
-            ButtonControl.Click += (sender, e) => this.Click?.Invoke(sender, e);
+            ButtonControl.Click += (sender, e) => Click?.Invoke(sender, e);
         }
 
         public event RoutedEventHandler Click;
 
         public string Header
         {
-            get { return (string)GetValue(HeaderProperty); }
-            set { SetValue(HeaderProperty, value); }
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
         }
-
-        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
+        public static DependencyProperty HeaderProperty { get; } = DependencyProperty.Register("Header", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
 
         public string Icon
         {
-            get { return (string)GetValue(IconProperty); }
-            set { SetValue(IconProperty, value); }
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
         }
-
-        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
+        public static DependencyProperty IconProperty { get; } = DependencyProperty.Register("Icon", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
 
         public FontFamily IconFontFamily
         {
-            get { return (FontFamily)GetValue(IconFontFamilyProperty); }
-            set { SetValue(IconFontFamilyProperty, value); }
+            get => (FontFamily)GetValue(IconFontFamilyProperty);
+            set => SetValue(IconFontFamilyProperty, value);
         }
-
-        public static readonly DependencyProperty IconFontFamilyProperty = DependencyProperty.Register("IconFontFamily", typeof(FontFamily), typeof(InboxButton), new PropertyMetadata((FontFamily)App.Current.Resources["SymbolThemeFontFamily"]));
+        public static DependencyProperty IconFontFamilyProperty { get; } = DependencyProperty.Register("IconFontFamily", typeof(FontFamily), typeof(InboxButton), new PropertyMetadata((FontFamily)App.Current.Resources["SymbolThemeFontFamily"]));
 
         public string Description
         {
-            get { return (string)GetValue(DescriptionProperty); }
-            set { SetValue(DescriptionProperty, value); }
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
         }
-
-        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register("Description", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
+        public static DependencyProperty DescriptionProperty { get; } = DependencyProperty.Register("Description", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
 
         public string ActionIcon
         {
-            get { return (string)GetValue(ActionIconProperty); }
-            set { SetValue(ActionIconProperty, value); }
+            get => (string)GetValue(ActionIconProperty);
+            set => SetValue(ActionIconProperty, value);
         }
-
-        public static readonly DependencyProperty ActionIconProperty = DependencyProperty.Register("ActionIcon", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
+        public static DependencyProperty ActionIconProperty { get; } = DependencyProperty.Register("ActionIcon", typeof(string), typeof(InboxButton), new PropertyMetadata(string.Empty));
     }
 }
